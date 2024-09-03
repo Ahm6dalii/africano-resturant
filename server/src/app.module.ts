e424e04a7669d407c9cbb135d1efabd7c68d5be0
Ahmed Ali <ahmedalielian20@gmail.com>
2024-09-03 20:08:04 +0300

    add atlas connection string

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot("mongodb+srv://ahmedalielian20:C0bvgRQxLNKLlIux@affricano-cluster.4smjx.mongodb.net/affricano?retryWrites=true&w=majority&appName=affricano-cluster")],
   controllers: [AppController],
   providers: [AppService],
 })
