b8140c7a78922cd4b709b3690d771bef17e53274
Mostafa-M-Abbas <you@example.com>
2024-09-30 12:56:47 +0300

    Update Password and Information

--- client/user/src/pages/setting/updatePassword/UpdatePassword.jsx
+++ client/user/src/pages/setting/updatePassword/UpdatePassword.jsx
@@ -4,30 +4,35 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { TextInput, Button, Label, List } from "flowbite-react";
 import axios from "axios";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import SettingIcon from "../../../components/ReactI-cons/setting/settingIcon";
 import LockIcon from "../../../components/ReactI-cons/lockIcon/LockIcon";
-import { HiXCircle } from "react-icons/hi";
-import { toast } from 'react-hot-toast';
+import { HiXCircle, HiEye, HiEyeOff } from "react-icons/hi";
+import { toast } from "react-hot-toast";
 import { Helmet } from "react-helmet-async";
-import SettingIcon from './../../../components/ReactI-cons/setting/SettingIcon';
-
-// Schema using Yup
-
 
 const UpdatePassword = () => {
   // localization
   const { translation } = useSelector((state) => state.lang);
-  const { user, userInfo } = useSelector((state) => state.auth);
-  const { link } = useSelector(state => state.apiLink)
+  const { user } = useSelector((state) => state.auth);
+  const { link } = useSelector((state) => state.apiLink);
   const [isLoading, setLoading] = useState(false);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
+  // States to manage password visibility
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  // Schema using Yup
   const schema = yup.object({
     newPassword: yup
       .string()
       .required(translation.required_password)
       .min(8, translation.min_char_pass)
-      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, translation.match_pass),
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        translation.match_pass
+      ),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref("newPassword"), null], translation.match_confirm_pass)
@@ -46,32 +51,23 @@ const UpdatePassword = () => {
 
   // Handle form submission
   const onSubmit = async (data) => {
-    setLoading(true)
-
+    setLoading(true);
     try {
       // Send request to backend
       const response = await axios.put(
         `${link}/update-pasword`,
-        {
-          newPassword: data.newPassword,
-        },
-        {
-          headers: {
-            token: `${user}`,
-          },
-        }
+        { newPassword: data.newPassword },
+        { headers: { token: `${user}` } }
       );
-
       // Display success message
-      toast.success(translation.passwordUpdated)
-      setLoading(false)
+      toast.success(translation.passwordUpdated);
+      setLoading(false);
     } catch (error) {
       // Handle error
-      setLoading(false)
+      setLoading(false);
       if (error?.response?.data?.message) {
-        toast.error(translation.passwordUpdateFail)
+        toast.error(translation.passwordUpdateFail);
       }
-
     }
   };
 
@@ -81,36 +77,49 @@ const UpdatePassword = () => {
         <title>Settings/updatePassword</title>
         <meta name="description" content="About Page" />
       </Helmet>
-      <h2 className="flex items-center gap-2  justify-center text-3xl font-semibold text-center mb-6  dark:text-orange-200">
+      <h2 className="flex items-center gap-2 justify-center text-3xl font-semibold text-center mb-6 dark:text-orange-200">
         <SettingIcon />
         {translation.change_pass}
       </h2>
-
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="max-w-md mx-auto  p-8 rounded-lg "
+        className="max-w-md mx-auto p-8 rounded-lg"
       >
-
         {/* New Password */}
         <div className="mb-4">
           <div className="flex items-center gap-1">
             <LockIcon />
             <Label htmlFor="newPassword" value={translation.new_pass} />
           </div>
-          <TextInput
-            id="newPassword"
-            name="newPassword"
-            type="password"
-            color={'white'}
-            className="text-black"
-            placeholder={translation.new_pass}
-            {...register("newPassword")}
-          />
-          {errors.newPassword &&
-            <List   >
-              <List.Item className='text-red-600 flex  text-sm  dark:text-red-500 capitalize mb-2' icon={HiXCircle}>{errors.newPassword?.message}</List.Item>
+          <div className="relative">
+            <TextInput
+              id="newPassword"
+              name="newPassword"
+              type={showNewPassword ? "text" : "password"}
+              color={"white"}
+              className="text-black"
+              placeholder={translation.new_pass}
+              {...register("newPassword")}
+              style={{ paddingRight: "2.5rem" }} // Add padding to the right
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+              onClick={() => setShowNewPassword(!showNewPassword)}
+            >
+              {showNewPassword ? <HiEyeOff /> : <HiEye />}
+            </button>
+          </div>
+          {errors.newPassword && (
+            <List>
+              <List.Item
+                className="text-red-600 flex text-sm dark:text-red-500 capitalize mb-2"
+                icon={HiXCircle}
+              >
+                {errors.newPassword?.message}
+              </List.Item>
             </List>
-          }
+          )}
         </div>
 
         {/* Confirm New Password */}
@@ -119,27 +128,49 @@ const UpdatePassword = () => {
             <LockIcon />
             <Label htmlFor="confirmPassword" value={translation.confirm_pass} />
           </div>
-          <TextInput
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            color={'white'}
-            className="text-black"
-            placeholder={translation.confirm_pass}
-            {...register("confirmPassword")}
-          />
-          {errors.confirmPassword &&
-            <List   >
-              <List.Item className='text-red-600 flex  text-sm  dark:text-red-500 capitalize mb-2' icon={HiXCircle}>{errors.confirmPassword?.message}</List.Item>
+          <div className="relative">
+            <TextInput
+              id="confirmPassword"
+              name="confirmPassword"
+              type={showConfirmPassword ? "text" : "password"}
+              color={"white"}
+              className="text-black"
+              placeholder={translation.confirm_pass}
+              {...register("confirmPassword")}
+              style={{ paddingRight: "2.5rem" }} // Add padding to the right
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            >
+              {showConfirmPassword ? <HiEyeOff /> : <HiEye />}
+            </button>
+          </div>
+          {errors.confirmPassword && (
+            <List>
+              <List.Item
+                className="text-red-600 flex text-sm dark:text-red-500 capitalize mb-2"
+                icon={HiXCircle}
+              >
+                {errors.confirmPassword?.message}
+              </List.Item>
             </List>
-          }
+          )}
         </div>
 
         {/* Submit Button */}
         <div className="text-center">
-          <Button disabled={isLoading} type="submit" gradientDuoTone="purpleToBlue">
-            {isLoading ? <i className="fa-solid fa-spin fa-spinner"></i> : translation.change_pass_btn}
-
+          <Button
+            disabled={isLoading}
+            type="submit"
+            gradientDuoTone="purpleToBlue"
+          >
+            {isLoading ? (
+              <i className="fa-solid fa-spin fa-spinner"></i>
+            ) : (
+              translation.change_pass_btn
+            )}
           </Button>
         </div>
       </form>
